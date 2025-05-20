e4100fcb6925505d59b4d8c7b426336b5da8a65d
Grayyy1219 <lance.musngi@gmail.com>
2025-05-20 15:59:02 +0800

    update mail.php

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
