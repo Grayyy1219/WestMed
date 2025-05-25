aec004a44de2f0da3c82fb5cb3d68fb1df35e2ae
Grayyy1219 <lance.musngi@gmail.com>
2025-05-26 00:43:37 +0800

    update logout.php

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ $query = mysqli_query($con, "UPDATE currentuser SET username = '0' WHERE userid 
 if ($query) {
     echo '<script>alert("Logout successfully");</script>';
     echo '<script>window.location.href = "Landing page.php";</script>';
+    session_destroy();
 } else {
     echo "Update failed: " . mysqli_error($con);
     echo '<script>window.location.href = "Landing page.php";</script>';
